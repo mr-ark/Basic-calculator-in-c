100b02995fb8ee7021e37a5956e7fdb8e0f3aefc
Annuay Raj-k [Ark] <100431877+mr-ark@users.noreply.github.com>
2023-02-03 19:31:32 +0530

    Update Main.c

--- Main.c
+++ Main.c
@@ -2,22 +2,26 @@
 #include <stdlib.h>
 
 int main(void) {
-    int num1,num2,choice,result;
-	printf("Enter 2 numbers");
+    int num1,num2,choice;
+    float result;
+	printf("Enter 2 numbers\n:");
     scanf("%d%d",&num1,&num2);
-    printf("1 for addition \n2 for subtraction \n3 for multiplication \n4for dividion \n         Enter your choice from above\n:");
+    printf("1 for addition \n2 for subtraction \n3 for multiplication \n4 for dividion \n\nEnter your choice from above\n:");
     scanf("%d",&choice);
     if(choice==1){
         result=num1+num2;
+        printf("result=%f", result);
     }else if(choice==2){
         result=num1-num2;
+        printf("result=%f", result);
     }else if(choice==3){
         result=num1*num2;
+        printf("result=%f", result);
     }else if(choice==4){
         result=num1/num2;
+        printf("result=%f", result);
     }else{
         printf("Noting for you!");
     }
-    printf("result=%d", result);
 	return EXIT_SUCCESS;
 }
